3ea4ac4b8bce1de63ede5ea0e7c9ffc9ffdb90fb
Tejas-Naik <rntejas2005@gmail.com>
2025-02-27 16:47:56 +0530

    --

--- middleware.ts
+++ middleware.ts
@@ -1,18 +1,19 @@
-import { authMiddleware } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+import { withClerkMiddleware } from "@clerk/nextjs/server";
 
 // /video/{id}/analysis
 // This example protects all routes including api/trpc routes
 // Please edit this to allow other routes to be public as needed.
-export default authMiddleware({
-  publicRoutes: ["/", "/sign-in", "/api/webhook"],
-  debug: true,
+export default withClerkMiddleware(() => {
+  return NextResponse.next();
 });
 
 export const config = {
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
-    "/((?!.+\\.[\\w]+$|_next).*)",
+    "/((?!.*\\..*|_next).*)",
     // Optional: Allow images to be accessed publicly
+    "/",
     "/(api|trpc)(.*)",
   ],
 };
