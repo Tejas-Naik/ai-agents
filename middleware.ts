797eb29238653bd23e23cacc3450ad05fc481931
Tejas-Naik <rntejas2005@gmail.com>
2025-02-27 16:59:46 +0530

    0

--- middleware.ts
+++ middleware.ts
@@ -1,21 +1,43 @@
-import { NextResponse } from "next/server";
-import { authMiddleware } from "@clerk/nextjs";
+// import { NextResponse } from "next/server";
+// import { auth } from "@clerk/nextjs";
+
+// // /video/{id}/analysis
+// // This example protects all routes including api/trpc routes
+// // Please edit this to allow other routes to be public as needed.
+// export default auth();
+
+// export const config = {
+//   matcher: [
+//     /*
+//      * Match all request paths except for the ones starting with:
+//      * - _next
+//      * - static (static files)
+//      * - favicon.ico (favicon file)
+//      * - public (public files)
+//      * - api (API routes)
+//      */
+//     "/((?!static|.*\\..*|_next|favicon.ico|api).*)",
+//   ],
+// };
+
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 // /video/{id}/analysis
-// This example protects all routes including api/trpc routes
-// Please edit this to allow other routes to be public as needed.
-export default authMiddleware();
+const isProtectedRoute = createRouteMatcher(["/video(.*)"]);
+
+export default clerkMiddleware(async (auth, req) => {
+  const { userId, redirectToSignIn } = await auth();
+
+  if (!userId && isProtectedRoute(req)) {
+    return redirectToSignIn();
+  }
+});
 
 export const config = {
   matcher: [
-    /*
-     * Match all request paths except for the ones starting with:
-     * - _next
-     * - static (static files)
-     * - favicon.ico (favicon file)
-     * - public (public files)
-     * - api (API routes)
-     */
-    "/((?!static|.*\\..*|_next|favicon.ico|api).*)",
+    // Skip Next.js internals and all static files, unless found in search params
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    // Always run for API routes
+    "/(api|trpc)(.*)",
   ],
 };
