27e9bcb12a7873bd5538a5b6dd15981476d7c14c
Tejas-Naik <rntejas2005@gmail.com>
2025-02-27 16:40:23 +0530

    ....

--- middleware.ts
+++ middleware.ts
@@ -1,24 +1,11 @@
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { authMiddleware } from "@clerk/nextjs";
 
 // /video/{id}/analysis
-const isProtectedRoute = createRouteMatcher(["/video(.*)"]);
-
-export default clerkMiddleware({
+// This example protects all routes including api/trpc routes
+// Please edit this to allow other routes to be public as needed.
+export default authMiddleware({
   publicRoutes: ["/api/webhook", "/", "/sign-in"],
   debug: true,
-  beforeAuth: (req) => {
-    // Check if the route requires authentication
-    if (isProtectedRoute(req)) {
-      return;
-    }
-  },
-  afterAuth: (auth, req) => {
-    const { userId } = auth;
-    // If the user is not signed in and the route requires authentication
-    if (!userId && isProtectedRoute(req)) {
-      return auth.redirectToSignIn();
-    }
-  },
 });
 
 export const config = {
